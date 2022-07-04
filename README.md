51f63af0b161c826ef09e89e0b5a2dbdf329e0e8
Jeremiah Flaga <flaga.jeremiah@gmail.com>
2022-07-05 03:58:11 +0800

    finished todo app tutorial

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # TodoApp.Backend.AspNetWebApi
+
+Resources:
+
+Create a web API with ASP.NET Core - https://docs.microsoft.com/en-us/aspnet/core/tutorials/first-web-api?view=aspnetcore-6.0&tabs=visual-studio
---
+++ TodoApp/TodoApp.Web/Models/TodoItemDTO.cs
@@ -0,0 +1,17 @@
+﻿namespace TodoApp.Web.Models
+{
+    public class TodoItemDTO
+    {
+        public long Id { get; set; }
+        public string? Name { get; set; }
+        public bool IsComplete { get; set; }
+
+        public static TodoItemDTO ItemToDTO(TodoItem todoItem) =>
+            new TodoItemDTO
+            {
+                Id = todoItem.Id,
+                Name = todoItem.Name,
+                IsComplete = todoItem.IsComplete
+            };
+    }
+}
